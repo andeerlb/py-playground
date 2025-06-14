e94230b278baf92746984b2993327a9a85bebcff
Anderson Babinski <andeerlb@gmail.com>
2025-06-14 20:58:55 -0300

    string

---
+++ basis/strings.py
@@ -0,0 +1,22 @@
+# %%
+doubleQuotes = "double quotes"
+simpleQuotes = 'simple quotes'
+
+# %%
+'a' + 'b'
+
+# %%
+'ab' * 4
+
+# %%
+'a' - 'b'
+
+# %%
+'It\'s an escaped quote!'
+"I'm a so-called \"script kiddie\""
+
+# %%
+"""This is line 1,
+... this is line 2,
+... this is line 3."""
+'This is line 1,\nthis is line 2,\nthis is line 3.'
