36b46aca4d6c19c4251f3583ddf354a29aebe990
Anderson Babinski <andeerlb@gmail.com>
2025-06-22 18:45:51 -0300

    iterator

---
+++ basis/iterator.py
@@ -0,0 +1,32 @@
+# An iterator is an object that can be iterated upon, meaning that you can traverse through all the values.
+# To create an object/class as an iterator you have to implement the methods __iter__() and __next__() to your object.
+# The __iter__() method acts similar, you can do operations (initializing etc.), but must always return the iterator object itself.
+# The __next__() method also allows you to do operations, and must return the next item in the sequence.
+# %%
+
+class MyNumbers:
+  def __iter__(self):
+    self.a = 1
+    return self
+
+  def __next__(self):
+    if self.a <= 20:
+      x = self.a
+      self.a += 1
+      return x
+    else:
+      raise StopIteration
+
+
+myclass = MyNumbers()
+myiter = iter(myclass)
+
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+
+for x in iter(myclass):
+  print(x);
+# %%
