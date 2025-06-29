f2765f5e6227ae94d7f5767f5b383e6f1b5c21bb
Anderson Babinski <andeerlb@gmail.com>
2025-06-28 21:07:12 -0300

    created new views and used abstract

--- django/polls/views.py
+++ django/polls/views.py
@@ -1,22 +1,55 @@
-from django.http import HttpResponse, HttpRequest
-from .models import Question
+from django.http import HttpResponse, HttpRequest, HttpResponseRedirect
+from .models import Question, Choice
 from django.shortcuts import get_object_or_404, render
-from django.http import Http404
+from django.db.models import F
+from django.urls import reverse
+from django.views import generic
 
-def detail(request: HttpRequest, question_id: int):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
+
+# def index(request: HttpRequest):
+#     latest_question_list = Question.objects.order_by("-pub_date")[:5]
+#     context = {"latest_question_list": latest_question_list}
+#     return render(request, "polls/index.html", context)
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+# def detail(request: HttpRequest, question_id: int):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, "polls/detail.html", {"question": question})
 
 
-def results(request: HttpRequest, question_id: int):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
+# def results(request: HttpRequest, question_id: int):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, "polls/results.html", {"question": question})
 
-def vote(request: HttpRequest, question_id: int):
-    return HttpResponse("You're voting on question %s." % question_id)
-
-def index(request: HttpRequest):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {"latest_question_list": latest_question_list}
-    return render(request, "polls/index.html", context)
+def vote(request: HttpRequest, question_id: int) -> HttpResponse:
+    question: Question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice: Choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
+    else:
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
--- django/sample/settings.py
+++ django/sample/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+from typing import List
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-uy76zpa$+y_)g7@&*edufx&+5p#7i54qf_o*ty&lxw_55u5gk(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS: List[str] = []
 
 
 # Application definition
