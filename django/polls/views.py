997d1d596703a112cc6b490f9840d81ad69d5ff9
Anderson Babinski <andeerlb@gmail.com>
2025-06-30 09:24:54 -0300

    writting tests to project.

--- django/polls/views.py
+++ django/polls/views.py
@@ -4,14 +4,20 @@ from django.shortcuts import get_object_or_404, render
 from django.db.models import F
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 class IndexView(generic.ListView):
     template_name = "polls/index.html"
     context_object_name = "latest_question_list"
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by("-pub_date")[:5]
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[
+            :5
+        ]
 
 # def index(request: HttpRequest):
 #     latest_question_list = Question.objects.order_by("-pub_date")[:5]
@@ -21,6 +27,12 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
+    
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 # def detail(request: HttpRequest, question_id: int):
 #     question = get_object_or_404(Question, pk=question_id)
