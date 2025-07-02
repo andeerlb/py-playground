319e4278b09ae69189db9247ce08aa0af3982735
Anderson Babinski <andeerlb@gmail.com>
2025-07-02 18:59:08 -0300

    views

---
+++ py-django/views.py
@@ -0,0 +1,52 @@
+# %%
+# writing async view
+import datetime
+from django.http import HttpResponse
+
+
+async def current_datetime(request):
+    now = datetime.datetime.now()
+    html = '<html lang="en"><body>It is now %s.</body></html>' % now
+    return HttpResponse(html)
+
+# %%
+# custom error views
+from django.core.exceptions import PermissionDenied
+from django.http import HttpResponse
+from django.test import SimpleTestCase, override_settings
+from django.urls import path
+
+
+def response_error_handler(request, exception=None):
+    return HttpResponse("Error handler content", status=403)
+
+
+def permission_denied_view(request):
+    raise PermissionDenied
+
+
+urlpatterns = [
+    path("403/", permission_denied_view),
+]
+
+handler403 = response_error_handler
+
+
+# ROOT_URLCONF must specify the module that contains handler403 = ...
+@override_settings(ROOT_URLCONF=__name__)
+class CustomErrorHandlerTests(SimpleTestCase):
+    def test_handler_renders_template_response(self):
+        response = self.client.get("/403/")
+        # Make assertions on the response here. For example:
+        self.assertContains(response, "Error handler content", status_code=403)
+
+# %%
+# a simple view
+from django.http import HttpResponse
+import datetime
+
+
+def current_datetime(request):
+    now = datetime.datetime.now()
+    html = '<html lang="en"><body>It is now %s.</body></html>' % now
+    return HttpResponse(html)
