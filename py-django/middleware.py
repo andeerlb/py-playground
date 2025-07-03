a10869d62cde9dd3b92ebdc121a0002e30245f03
Anderson Babinski <andeerlb@gmail.com>
2025-07-03 18:56:19 -0300

    async middleware

--- py-django/middleware.py
+++ py-django/middleware.py
@@ -46,3 +46,46 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "SimpleMiddleware"
 ]
+
+# %% 
+
+# async Middleware
+from asgiref.sync import iscoroutinefunction
+from django.utils.decorators import sync_and_async_middleware
+
+
+@sync_and_async_middleware
+def simple_middleware(get_response):
+    if iscoroutinefunction(get_response):
+
+        async def middleware(request):
+            response = await get_response(request)
+            return response
+
+    else:
+
+        def middleware(request):
+            response = get_response(request)
+            return response
+
+    return middleware
+
+# %%
+# class based middleware with async behavior
+
+from asgiref.sync import iscoroutinefunction, markcoroutinefunction
+
+
+class AsyncMiddleware:
+    async_capable = True
+    sync_capable = False
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+        if iscoroutinefunction(self.get_response):
+            markcoroutinefunction(self)
+
+    async def __call__(self, request):
+        response = await self.get_response(request)
+        # Some logic ...
+        return response
