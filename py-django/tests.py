5edcdf8e48140fddcaf31ba16fef6ebf84f591c7
Anderson Babinski <andeerlb@gmail.com>
2025-07-09 21:42:12 -0300

    tests

---
+++ py-django/tests.py
@@ -0,0 +1,21 @@
+#### Django’s unit tests use a Python standard library module: unittest. This module defines tests using a class-based approach.
+#### Once you’ve written tests, run them using the test command of your project’s manage.py utility:
+#### `./manage.py test`
+
+# %%
+from django.test import TestCase
+from myapp.models import Animal
+
+
+class AnimalTestCase(TestCase):
+    def setUp(self):
+        Animal.objects.create(name="lion", sound="roar")
+        Animal.objects.create(name="cat", sound="meow")
+
+    def test_animals_can_speak(self):
+        """Animals that can speak are correctly identified"""
+        lion = Animal.objects.get(name="lion")
+        cat = Animal.objects.get(name="cat")
+        self.assertEqual(lion.speak(), 'The lion says "roar"')
+        self.assertEqual(cat.speak(), 'The cat says "meow"')
+# %%
